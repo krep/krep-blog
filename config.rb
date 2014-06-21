b5c75134dbf81390e312207b321c666a3cdcd27a
Aurélien Léger <enyriel@gmail.com>
2014-06-21 19:35:09 +0200

    Fix unless statement for current page title.

--- config.rb
+++ config.rb
@@ -92,7 +92,7 @@ end
 helpers do
   def page_title
     return current_page.data.title + ' &ndash; ' + blog_title unless current_page.data.title.nil?
-    return current_article.title + ' &ndash; ' + blog_title unless current_article.nil? && current_article.title
+    return current_article.title + ' &ndash; ' + blog_title if !current_article.nil? && current_article.title
     blog_title
   end
 end
